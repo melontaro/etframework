b554e90c60babd14e1fd866142c17f2b18d4269c
xuyin <axuyin@gmail.com>
2021-01-26 11:27:03 +0800

    修复客户端BUG

--- Unity/Assets/Code/ET@hotfix/Base/Helper/ExceptionHelper.cs
+++ Unity/Assets/Code/ET@hotfix/Base/Helper/ExceptionHelper.cs
@@ -3,15 +3,11 @@ using ETModel;
 
 namespace ETHotfix
 {
-	public static class ExceptionHelper
-	{
-		public static string ToStr(this Exception exception)
-		{
-#if ILRuntime
-			return $"{exception.Data["StackTrace"]} \n\n {exception}";
-#else
-			return exception.ToString();
-#endif
-		}
-	}
+    public static class ExceptionHelper
+    {
+        public static string ToStr(this Exception exception)
+        {
+            return $"{exception.Data["StackTrace"]} \n\n {exception}";
+        }
+    }
 }
